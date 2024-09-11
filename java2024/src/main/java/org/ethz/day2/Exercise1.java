2f28a8b08f784dc3e8b9b7b707b692ef48b60be0
balacmi <milos.balac@ivt.baug.ethz.ch>
2024-09-11 09:06:12 +0200

    adding solutions to day2 exercises

---
+++ java2024/src/main/java/org/ethz/day2/Exercise1.java
@@ -0,0 +1,17 @@
+package org.ethz.day2;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		char ch = input.next().charAt(0);		
+		int n = ch;
+		//we want to convert n to hex representation
+		System.out.println("\\" + Integer.toHexString(n));	
+		input.close();
+	}
+
+}
---
+++ java2024/src/main/java/org/ethz/day2/Exercise4.java
@@ -0,0 +1,26 @@
+package org.ethz.day2;
+
+import java.util.Scanner;
+
+public class Exercise4 {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		int number = input.nextInt();
+		System.out.println(isPrime(number));
+		input.close();
+	}
+
+	public static boolean isPrime(int n) {
+		
+		int i = 2;
+		while (i <= n / 2) {
+			
+			if (n % i == 0)
+				return false;
+			i++;
+		}
+		return true;
+	}
+}
